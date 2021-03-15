e08010b89c08de0c80d4becc80662ff126010492
ercbk <ericbookax0@yahoo.com>
2021-03-14 22:49:10 -0400

    Added datasets and docs

---
+++ R/data.R
@@ -0,0 +1,143 @@
+#' Historical Weekly COVID-19 Cases by Age for Indiana
+#'
+#' Processed data for Demographics [heatmaps](https://ercbk.github.io/Indiana-COVID-19-Website/demographics.html#cases-by-age) that shows a breakdown of weekly COVID-19 cases by age group.
+#'
+#' @examples
+#' head(age_cases_heat)
+"age_cases_heat"
+
+
+#' Historical Weekly COVID-19 Deaths by Age Group for Indiana
+#'
+#' Processed data for Demographics [line charts](https://ercbk.github.io/Indiana-COVID-19-Website/demographics.html#deaths-by-age) that shows counts and trends of weekly COVID-19 deaths by age group.
+#'
+#'
+#' @examples
+#' head(age_death_line)
+"age_death_line"
+
+
+#' Historical Daily COVID-19 Hospital Admissions by Age Group for Indiana
+#'
+#' Raw data for the Hospitals [line charts](https://ercbk.github.io/Indiana-COVID-19-Website/hospitals.html#state-hospital-mortality-staffing-shortages-admissions) that shows daily COVID-19 hospital admissions by age group and gender.
+#'
+#' @examples
+#' head(age_hosp_line)
+"age_hosp_line"
+
+
+#' Historical Daily ICU Beds and Ventilators for Indiana
+#'
+#' Raw data for the Static Charts [Hospitalizations, ICU Beds and Ventilator Availability](https://ercbk.github.io/Indiana-COVID-19-Website/static.html#Hospitalizations,_ICU_Beds_and_Ventilator_Availability) that shows resource counts of ICU beds and ventilators.
+#'
+#' @examples
+#' head(beds_vents_complete)
+"beds_vents_complete"
+
+
+#' Historical State Hospital Staff Shortages, Hospital Mortality Rate, Hospital Admissions and Ages Skewness of Admissions for Indiana
+#'
+#' Processed data for Hospitals [line charts](https://ercbk.github.io/Indiana-COVID-19-Website/hospitals.html#state-hospital-mortality-staffing-shortages-admissions) that shows state hospital staff shortages, hospital mortality rate, hospital admissions, and age skewness of hospital admissions towards older patients.
+#'
+#' @examples
+#' head(hosp_msas_line)
+"hosp_msas_line"
+
+
+#' Local Hospital Capacity for Indiana
+#'
+#' Processed data for the Hospitals [Local Hospital Capacity table](https://ercbk.github.io/Indiana-COVID-19-Website/hospitals.html#local-hospital-capacity) that shows capacity measures and counts of COVID-19 patients for hospitals at the local level.
+#'
+#' @examples
+#' head(hosp_react_tab)
+"hosp_react_tab"
+
+
+#' Historical Daily Tests, Cases and Deaths by Age for Indiana
+#'
+#' Raw data for Demographics [line charts](https://ercbk.github.io/Indiana-COVID-19-Website/demographics.html) that shows daily case and death counts by age group.
+#'
+#' @examples
+#' head(ind_age_complete)
+"ind_age_complete"
+
+
+#' Historical Daily Cases and Deaths by Race for Indiana
+#'
+#' Raw data that shows daily case and death counts by race.
+#'
+#' @examples
+#' head(ind_race_complete)
+"ind_race_complete"
+
+
+#' Historical Weekly Median Age of Cases, Weekly Tests and Deaths for Indiana
+#'
+#' Processed data for the Demographics [bubble chart](https://ercbk.github.io/Indiana-COVID-19-Website/demographics.html#median-age-of-cases-tests-deaths) that shows the number of tests, number of deaths, and median age of the cases per week.
+#'
+#' @examples
+#' head(median_age_bubble)
+"median_age_bubble"
+
+
+#' Historical Daily Hospital Admissions and Deaths for Indiana
+#'
+#' Raw data for the Hospitals [line charts](https://ercbk.github.io/Indiana-COVID-19-Website/hospitals.html#state-hospital-mortality-staffing-shortages-admissions) that shows daily COVID-19 hospital admissions, hospital deaths, and hospital death rate.
+#'
+#' @examples
+#' head(mort_hosp_line)
+"mort_hosp_line"
+
+
+#' Historical Weekly Positivity Rates and Daily Cases per 100,000 Indiana Residents
+#'
+#' Processed data for the [Indiana COVIDcast Dashboard](https://ercbk.github.io/Indiana-COVIDcast-Dashboard/#dashboard) that shows weekly positivity rate and daily cases per 100,000 residents per metropolitan statistical area.
+#'
+#' @examples
+#' head(msa_cases100_posrate_historic)
+"msa_cases100_posrate_historic"
+
+
+#' Historical Daily Values of Delphi Research Groups Combined Indicator for Indiana
+#'
+#' Processed data for the [Indiana COVIDcast Dashboard](https://ercbk.github.io/Indiana-COVIDcast-Dashboard/#dashboard) that shows historic daily values of Delphi Research Group's Combined Indicator for each methropolitan statistical area.
+#'
+#' @examples
+#' head(dash_ci_line)
+"dash_ci_line"
+
+
+#' Historical Weekly COVID-19 Statistics and Counts for Illinois Counties
+#'
+#' Raw data for the [Indiana COVIDcast Dashboard](https://ercbk.github.io/Indiana-COVIDcast-Dashboard/#dashboard) that shows various weekly COVID-19 county statistics for Illinois
+#'
+#' @examples
+#' head(illinois_tests_complete)
+"illinois_tests_complete"
+
+
+#' Historical Daily COVID-19 Test Results for Indiana Counties
+#'
+#' Raw data for the [Indiana COVIDcast Dashboard](https://ercbk.github.io/Indiana-COVIDcast-Dashboard/#dashboard) that shows daily COVID-19 county test results for Indiana
+#'
+#' @examples
+#' head(ind_tests_complete)
+"ind_tests_complete"
+
+
+#' Historical Daily COVID-19 Test Results for Michigan Counties
+#'
+#' Raw data for the [Indiana COVIDcast Dashboard](https://ercbk.github.io/Indiana-COVIDcast-Dashboard/#dashboard) that shows daily COVID-19 county test results for Michigan
+#'
+#' @examples
+#' head(mich_tests_complete)
+"mich_tests_complete"
+
+
+#' Historical Daily COVID-19 Test Results for Wisconsin Counties
+#'
+#' Raw data for the [Indiana COVIDcast Dashboard](https://ercbk.github.io/Indiana-COVIDcast-Dashboard/#dashboard) that shows daily COVID-19 county test results for Wisconsin
+#'
+#' @examples
+#' head(wisc_tests_complete)
+"wisc_tests_complete"
